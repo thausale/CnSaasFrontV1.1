98fc92f22cffd1b681d39d7db7f482427779b1fc
Niels Cnops <cnops_niels@hotmail.com>
2023-05-11 16:14:07 +0200

    feat: added date to clock component

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -5,6 +5,7 @@
     export let format = "hms";
   
     let time = getTime();
+    let date = getDate()
   
     function getTime() {
       const now = new Date();
@@ -12,23 +13,68 @@
       const hours = now.getHours().toString().padStart(2, "0");
       const minutes = now.getMinutes().toString().padStart(2, "0");
       const seconds = now.getSeconds().toString().padStart(2, "0");
+      const day = now.getDay().toString()
+      const month = now.getMonth().toString()
+      const year = now.getFullYear().toString
   
       switch (format) {
         case "hms":
-          return `${hours}:${minutes}:${seconds}`;
+          return `
+            ${hours}:${minutes}:${seconds}
+
+          `;
         case "hm":
           return `${hours}:${minutes}`;
         case "ms":
           return `${minutes}:${seconds}`;
       }
     }
+
+    function getDate(){
+      const daysOfWeek = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun',
+];
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+
+
+      const now = new Date()
+      const day = now.getDay()-1
+      const dayString = daysOfWeek[day]
+      const dayNumber = now.getDate()
+      const month = monthNames[now.getMonth()];
+
+      return `${dayString} ${dayNumber} ${month}  `    }
   
     setInterval(() => {
       time = getTime();
+    }, 1000);
+    setInterval(() => {
+      date = getDate();
     }, 1000);
   </script>
   
   <div style=" padding: 5px;">
     <p>{time}</p>
+    <p>{date}</p>
   </div>
   

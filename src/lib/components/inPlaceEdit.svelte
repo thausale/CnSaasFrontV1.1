05a468d76c5b85e5a8860b0702508472f4d08f56
Niels Thuis <cnops_niels@hotmail.com>
2023-05-15 21:38:48 +0200

    feat: editFields on profile page

---
+++ src/lib/components/inPlaceEdit.svelte
@@ -0,0 +1,61 @@
+<script>
+// @ts-nocheck
+
+    import { createEventDispatcher, onMount } from 'svelte'
+  
+    export let value, required = true
+  
+    const dispatch = createEventDispatcher()
+    let editing = false, original
+  
+    onMount(() => {
+      original = value
+    })
+  
+    function edit() {
+      editing = true
+    }
+  
+    function submit() {
+          if (value != original) {
+              dispatch('submit', value)
+          }
+          
+      editing = false
+    }
+  
+    function keydown(event) {
+      if (event.key == 'Escape') {
+        event.preventDefault()
+        value = original
+        editing = false
+      }
+    }
+      
+      function focus(element) {
+          element.focus()
+      }
+  </script>
+  
+  {#if editing}
+    <form on:submit|preventDefault={submit} on:keydown={keydown}>
+      <input bind:value on:blur={submit} {required} use:focus/>
+    </form>
+  {:else}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div on:click={edit}>
+      {value}
+    </div>
+  {/if}
+  
+  <style>
+    input {
+      border: none;
+      background: none;
+      font-size: inherit;
+      color: inherit;
+      font-weight: inherit;
+      text-align: inherit;
+      box-shadow: none;
+    }
+  </style>

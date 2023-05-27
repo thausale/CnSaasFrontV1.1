97a32a1612672eccf84146635f7aab173f094bb8
Niels Thuis <cnops_niels@hotmail.com>
2023-05-27 21:42:49 +0200

    feat: filtering !

--- src/routes/(app)/user-management/+page.svelte
+++ src/routes/(app)/user-management/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import { goto } from '$app/navigation';
+	// import { goto } from '$app/navigation';
 	import { User } from '$lib/components/stores.js';
 	import { onMount } from 'svelte';
 	import { apiClient } from '$lib/api/UserApi.js';
-	import { Accordion, AccordionItem, ProgressBar } from '@skeletonlabs/skeleton';
+	import { Accordion, AccordionItem, ProgressBar, InputChip } from '@skeletonlabs/skeleton';
 
 	let loading = false;
 	export let data;
@@ -13,15 +13,32 @@
 	import { Table } from '@skeletonlabs/skeleton';
 	import { tableMapperValues } from '@skeletonlabs/skeleton';
 
-	const table = {
+	let searchTerm = [];
+
+	$: filteredUsers =
+		searchTerm.length > 0
+			? users.filter((user) =>
+					searchTerm.some(
+						(term) =>
+							user.firstName.toLowerCase().includes(term.toLowerCase()) ||
+							user.lastName.toLowerCase().includes(term.toLowerCase()) ||
+							user.lastName.toLowerCase().includes(term.toLowerCase()) ||
+							user.email.toLowerCase().includes(term.toLowerCase())
+					)
+			  )
+			: users;
+
+	$: console.log(filteredUsers);
+
+	$: table = {
 		// A list of heading labels.
 		head: ['firstName', 'lastName', 'role', 'email'],
 		// The data visibly shown in your table body UI.
-		body: tableMapperValues(users, ['firstName', 'lastName', 'role_name', 'email']),
+		body: tableMapperValues(filteredUsers, ['firstName', 'lastName', 'role_name', 'email']),
 		// Optional: The data returned when interactive is enabled and a row is clicked.
-		meta: tableMapperValues(users, ['firstName', 'lastName', 'role_name', 'email', 'id']),
+		meta: tableMapperValues(filteredUsers, ['firstName', 'lastName', 'role_name', 'email', 'id']),
 		// Optional: A list of footer labels.
-		foot: ['Total', '', `<code class="code">${users.length}</code>`]
+		foot: ['Total', '', `<code class="code">${filteredUsers.length}</code>`]
 	};
 
 	function selectionHandler(e) {
@@ -34,5 +51,6 @@
 	<ProgressBar meter="variant-filled-primary" />
 {:else}
 	<!-- TODO: Add modal when clicked to edit user -->
+	<InputChip bind:value={searchTerm} name="chips" placeholder="Search users ..." class="m-4" />
 	<Table source={table} class=" p-4" interactive="true" on:selected={selectionHandler} />
 {/if}

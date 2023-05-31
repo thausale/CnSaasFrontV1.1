4443432cdb11b5a591c3196a7f451638a11de446
Niels Thuis <cnops_niels@hotmail.com>
2023-05-31 15:26:51 +0200

    feat: loader

--- src/lib/components/EnterDataForm.svelte
+++ src/lib/components/EnterDataForm.svelte
@@ -2,21 +2,44 @@
 	import { enhance } from '$app/forms';
 	import { DataApi } from '$lib/api/DataApi';
 	import { serverEventHandler } from '$lib/helpers/serverEvents';
+	import { invalidate } from '$app/navigation';
+	import { toastStore, ProgressRadial } from '@skeletonlabs/skeleton';
 
 	export let form;
 
+	$: console.log(form);
 	let name = 'test';
 	let batch = 'ml-23-158';
 	let alcohol = '3.9';
 	let EBC = '2';
 	let turbidity = '12';
 
+	let loading = false;
+
 	import { focusTrap } from '@skeletonlabs/skeleton';
+
+	const handleSubmit = () => {
+		loading = true;
+	};
+
+	$: if (form?.status === 201) {
+		const toastSettings = {
+			message: form.message,
+			timeout: 3000,
+			background: 'variant-filled-success'
+		};
+		toastStore.trigger(toastSettings);
+		loading = false;
+
+		invalidate('getData');
+	} else {
+		loading = false;
+	}
 </script>
 
 <main>
 	<h1>Beer Form</h1>
-	<form class="w-full" use:enhance use:focusTrap method="POST">
+	<form class="w-full" use:enhance use:focusTrap method="POST" on:submit={handleSubmit}>
 		{#if form?.error}
 			<p class="error" transition:blur>{form.error}</p>
 		{:else}
@@ -48,7 +71,13 @@
 			<input type="number" bind:value={turbidity} class="input" name="turbidity" />
 		</label>
 		<div class="flex items-center">
-			<button type="submit" class="btn variant-filled-primary max-w-full">Submit</button>
+			<button type="submit" class="btn variant-filled-primary max-w-full">
+				{#if loading}
+					<ProgressRadial width="w-6" />
+				{:else}
+					Submit
+				{/if}
+			</button>
 		</div>
 	</form>
 </main>

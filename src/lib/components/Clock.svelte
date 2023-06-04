b494710afaf3146daf1c75755ffca3e9f74cc4ff
Niels Thuis <cnops_niels@hotmail.com>
2023-06-04 12:00:54 +0200

    fix: sunday is the 0th day bug

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,80 +1,53 @@
 <script>
-    import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
-    import "@skeletonlabs/skeleton/styles/skeleton.css";
-  
-    export let format = "hms";
-  
-    let time = getTime();
-    let date = getDate()
-  
-    function getTime() {
-      const now = new Date();
-  
-      const hours = now.getHours().toString().padStart(2, "0");
-      const minutes = now.getMinutes().toString().padStart(2, "0");
-      const seconds = now.getSeconds().toString().padStart(2, "0");
-      const day = now.getDay().toString()
-      const month = now.getMonth().toString()
-      const year = now.getFullYear().toString
-  
-      switch (format) {
-        case "hms":
-          return `
-            ${hours}:${minutes}:${seconds}
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
 
-          `;
-        case "hm":
-          return `${hours}:${minutes}`;
-        case "ms":
-          return `${minutes}:${seconds}`;
-      }
-    }
+	let time = getTime();
+	let date = getDate();
 
-    function getDate(){
-      const daysOfWeek = [
-    'Mon',
-    'Tue',
-    'Wed',
-    'Thu',
-    'Fri',
-    'Sat',
-    'Sun',
-];
-const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ];
+	function getTime() {
+		const now = new Date();
+		const hours = now.getHours().toString().padStart(2, '0');
+		const minutes = now.getMinutes().toString().padStart(2, '0');
+		const seconds = now.getSeconds().toString().padStart(2, '0');
 
+		return `${hours}:${minutes}:${seconds}`;
+	}
 
+	function getDate() {
+		const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+		const monthNames = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
 
-      const now = new Date()
-      const day = now.getDay()-1
-      const dayString = daysOfWeek[day]
-      const dayNumber = now.getDate()
-      const month = monthNames[now.getMonth()];
+		const now = new Date();
+		const day = now.getDay() - 1;
+		const dayString = day == -1 ? 'sun' : daysOfWeek[day];
+		const dayNumber = now.getDate();
+		const month = monthNames[now.getMonth()];
+		return `${dayString} ${dayNumber} ${month}  `;
+	}
 
-      return `${dayString} ${dayNumber} ${month}  `    }
-  
-    setInterval(() => {
-      time = getTime();
-    }, 1000);
-    setInterval(() => {
-      date = getDate();
-    }, 1000);
-  </script>
-  
-  <div style=" padding: 5px;">
-    <p>{time}</p>
-    <p>{date}</p>
-  </div>
-  
+	setInterval(() => {
+		time = getTime();
+	}, 1000);
+	setInterval(() => {
+		date = getDate();
+	}, 1000);
+</script>
+
+<div style=" padding: 5px;">
+	<p>{time}</p>
+	<p>{date}</p>
+</div>

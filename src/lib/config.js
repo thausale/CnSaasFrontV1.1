549710cd61edc5b134633085fc0e2cb4cf1eb3c5
Niels Thuis <cnops_niels@hotmail.com>
2023-06-05 14:13:39 +0200

    fix: testing SSE live

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,6 @@
 // config.js
+import { PUBLIC_SSE_URL } from '$env/static/public';
+
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const serverAddress = isDevelopment ? 'http://localhost:3000' : '';
+export const serverAddress = isDevelopment ? 'http://localhost:3000' : PUBLIC_SSE_URL;

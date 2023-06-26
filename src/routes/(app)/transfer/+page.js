1e7e8b5b358e5383ec2d3e3dbf6020902e83b958
Niels Cnops <cnops_niels@hotmail.com>
2023-06-26 11:44:55 +0200

    Revert "fix: protection"
    This reverts commit 9c607763057ff75a278d0b04c30565ae5882da06.

--- src/routes/(app)/transfer/+page.js
+++ src/routes/(app)/transfer/+page.js
@@ -2,10 +2,8 @@ import { DataApi } from '$lib/api/DataApi.js';
 import { RoleController } from '$lib/api/RoleController.js';
 
 export async function load({ cookies, fetch, depends }) {
-	const authorization = cookies.get('AuthenticationToken');
-
 	const [entries, roles] = await Promise.all([
-		DataApi.getData(authorization),
+		DataApi.getData(),
 		RoleController.getAllRoles(fetch)
 	]);
 
